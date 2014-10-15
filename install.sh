83f5f87e56a0e2a00d0a2eeba57572b6a68dc279
jmechnich <joerg.mechnich@gmail.com>
2014-10-16 01:34:43 +0200

    prettified install script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-sudo python setup.py install
-sudo touch /usr/share/icons/hicolor
+#PREFIX=/usr/local
+PREFIX=/usr
+FILES=files.txt
+
+echo "Installing to $PREFIX, keeping list of files in $FILES"
+echo
+
+sudo python setup.py install --prefix "$PREFIX" --record "$FILES"
+sudo touch $PREFIX/share/icons/hicolor
+
+echo
+echo "Uninstall with 'cat $FILES | sudo xargs rm -rf'"
